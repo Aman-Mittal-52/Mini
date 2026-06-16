5301ca7d10bac86ab6a9e9d3dc96d9b5b500e20c
Aman Mittal <amanmittle1234@gmail.com>
2026-06-17 02:00:12 +0530

    Basic UI

---
+++ src/components/nav-main.tsx
@@ -0,0 +1,52 @@
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarInput,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Label } from "./ui/label"
+import { Search } from "lucide-react"
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon: React.ReactNode
+    isActive?: boolean
+  }[]
+}) {
+  return (
+    <SidebarMenu>
+       <SidebarGroup className="">
+        <SidebarGroupContent className="relative">
+          <Label htmlFor="search" className="sr-only">
+            Search
+          </Label>
+          <SidebarInput
+            id="search"
+            placeholder="Search the docs..."
+            className="pl-8"
+          />
+          <Search className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
+        </SidebarGroupContent>
+      </SidebarGroup>
+
+      {items.map((item) => (
+        <SidebarMenuItem key={item.title}>
+          <SidebarMenuButton asChild isActive={item.isActive}>
+            <a href={item.url}>
+              {item.icon}
+              <span>{item.title}</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+
+     
+    </SidebarMenu>
+  )
+}
